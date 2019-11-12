857f18991c9c5c49571463246fe6e17db5fd6149
yema <cz081596@163.com>
2019-11-12 18:10:13 +0800

    PC头条-初始化-准备资源

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui'
+import './styles/index.less'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
